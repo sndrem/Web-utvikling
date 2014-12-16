b6702dd266da46fda7735bb51ac4c33815cca561
Sindre <sndrem@live.no>
2014-12-17 00:15:14 +0100

    Virker fortsatt

--- Header til BVHK/carousel.js
+++ Header til BVHK/carousel.js
@@ -1,13 +1,18 @@
 $(document).ready(function(){
-$('#BVHK-slideshow').owlCarousel( {
-	loop: true,
-	margin: 10,
-	items: 4,
-	autoplay: true,
-	autoplayTimeout: 3000,
-	autoplayHoverPause: true,
-	dots: true,
-	dotsEach: true,
-	nav: true
-	});
+
+	$("#BVHK-slideshow").owlCarousel( {
+		loop: true,
+		items: 1,
+		nav: true,
+		dots: true,
+		dotsEach: true,
+		margin: 10,
+		autoplay: true,
+		autoplaySpeed: 10000,
+		autoplayHoverPause: true,
+		navText: ['Forrige', 'Neste'],
+		animateIn: 'fadeOutLeft'
+		
+		});
+
 });
